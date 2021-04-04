63dac2edf51156d8d8432c137f091e5e18335ee8
Kaustubh Debnath <kaustubh.debnath10@gmail.com>
2021-04-04 12:50:24 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -11,13 +11,18 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hiddenInset',
+    webPreferences: {
+      webSecurity: false
+    }
+   
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
